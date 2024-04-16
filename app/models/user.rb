cb1a65ca34ebce48d873b2717d108ea6645d1bd2
Mohamed Mansour <momans3011@gmail.com>
2024-04-16 23:33:08 +0200

    User MVC

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
     validates :name,presence: true,length: { maximum: 50}
     validates :email,presence: true, uniqueness: {case_sensitive: false},format: {with: URI::MailTo::EMAIL_REGEXP}
     has_secure_password
-    validates :password,presence: true,length: { minimum: 6, too_short: "minimum is 6 characters" }
+    validates :password,presence: true,length: { minimum: 6, too_short: "minimum is 6 characters" }, allow_nil: true #allow empty password on update
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :todos
-  root "todos#index"
+  resources :users
+  
+  # root "todos#index"
+  root "application#root"
+
+  get "/signup", to: "users#new"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
