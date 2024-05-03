901256cbc1377906c20413c7814485a226354b23
Mohamed Mansour <momans3011@gmail.com>
2024-05-03 23:25:40 +0300

    added user todos relation to database

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_31_152830) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_31_152830) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "todos", "users"
 end
